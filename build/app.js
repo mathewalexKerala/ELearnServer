83b16df98a795ecaaa2b7eda7ad3a9d1a2d3b68c
Mathew Alex <mathewalex557@gmail.com>
2024-03-15 15:46:36 +0530

    new commit

--- build/app.js
+++ build/app.js
@@ -23,7 +23,7 @@ exports.app.use(express_1.default.json({ limit: "50mb" }));
 exports.app.use((0, cookie_parser_1.default)());
 // cors => cross origin resource sharing
 exports.app.use((0, cors_1.default)({
-    origin: ["https://e-learning-client-kappa.vercel.app"],
+    origin: ["https://e-learning-client-sage.vercel.app"],
     credentials: true,
 }));
 // api requests limit
